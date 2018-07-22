d5c102b9681ea88fe142ea32ef9fb817638a6978
sxx <shixinxin@yit.com>
2018-07-22 23:16:03 +0800

    修改已调整好的文件

--- dubbo/dubbo-admin/docker-entrypoint.sh
+++ dubbo/dubbo-admin/docker-entrypoint.sh
@@ -12,9 +12,7 @@ then
   ROOT_PASSWD='123456'
 fi
 
-sed -i -r "s%([0-9]+\.){3}[0-9]+\:[0-9]+%${ZOO_SERVERS}%g" /usr/local/tomcat/webapps/dubbo-admin/WEB-INF/dubbo.properties
-
-sed -i -r "s%dubbo.admin.root.password=%dubbo.admin.root.password=${ROOT_PASSWD}%g" /usr/local/tomcat/webapps/dubbo-admin/WEB-INF/dubbo.properties
+sed -i -r "s%([0-9]+\.){3}[0-9]+\:[0-9]+%${ZOO_SERVERS}%g" /usr/local/tomcat/webapps/ROOT/WEB-INF/dubbo.properties
 
 cd /usr/local/tomcat/bin
 
